d99a5d01414c5b8185ca0894abcf6e6ee8f8c9c9
Darcks OMega <Darcks OMega@DarcksOMega-PC>
2016-02-13 21:02:25 -0500

    Creation of List and Test

--- Lista/src/List/Test.java
+++ Lista/src/List/Test.java
@@ -7,6 +7,20 @@ package List;
 
 public class Test {
     
-    
+    public static void main (String args[]){
+        
+        List L = new List();
+        
+        L.add(1, 0);
+        L.add(5, 1);
+        L.add(10, 2);
+        L.add(4, 3);
+        L.add(2, 4);
+        L.add(15, 5);
+        
+        System.out.print(" Initial Elements:  ");
+        L.print();
+        
+    }
     
 }
